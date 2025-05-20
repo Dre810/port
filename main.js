6a5772fc86cc134843f05d3678d89b899173a0a8
Davis Kamau <daviskamau810@gmail.com>
2025-05-20 22:36:22 +0300

    created two js files

---
+++ main.js
@@ -0,0 +1,14 @@
+//arrays - variables that hold multiple values
+
+const fruits = new Array('apple', 'banana', 'orange');
+
+fruits[3] = 'grapes';
+fruits.push('mango')
+fruits.unshift('strawberry');
+fruits.pop();
+console.log(Array.isArray('hey'));
+console.log(fruits.indexOf('banana'));
+console.log(fruits.reverse());
+
+
+console.log(fruits);
