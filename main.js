0cfeb46ebe2db22dbfe68e2d3a671435ab4cff98
Davis Kamau <daviskamau810@gmail.com>
2025-05-25 15:56:30 +0300

    for loop numbers in console

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-//arrays - variables that hold multiple values
+const todos = [
+    {
+        id: 1,
+        text: 'take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'do laundry',
+        isCompleted: false
+    },
+    {
+        id: 3,
+        text: 'buy groceries',
+        isCompleted: false
+    },
+    {
+        id: 4,
+        text: 'clean the house',
+        isCompleted: true
+    }
+];
 
-const fruits = new Array('apple', 'banana', 'orange');
+for(let i=0; i<todos.length; i++) {
+    console.log(`For Loop Number: ${i}`);
+    
+}
 
-fruits[3] = 'grapes';
-fruits.push('mango')
-fruits.unshift('strawberry');
-fruits.pop();
-console.log(Array.isArray('hey'));
-console.log(fruits.indexOf('banana'));
-console.log(fruits.reverse());
-
-
-console.log(fruits);
